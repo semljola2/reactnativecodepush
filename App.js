429f52fae13b023d8c899bc7ca48e59cacefe9a1
semljola2 <johan.lauri@gmail.com>
2017-11-30 14:22:59 +0100

    Minor changes to app name

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>My Fortum App Sweden!</Text>
+        <Text>MFAs Sweden!</Text>
       </View>
     );
   }
